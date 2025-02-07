be74e63885a10837964fa5aa712a7fb8ab9e0116
Soomin Jeong <sjeong0643@gmail.com>
2025-02-06 21:32:41 -0800

    Created a skeleton function code for POST and GET request

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
+class APIController {
+    constructor() {
+        this.xhttp = new XMLHttpRequest();
+    }
+
+    reqStoreWord(word, desc) {
+        this.xhttp.open("POST", "placeholderLink", true);
+        this.xhttp.send("word=" + word + "?desc=" + desc);
+        this.xhttp.onload = function() {
+            // Insert response onto store.html
+        }
+    }
+
+    storeWord() {
+        const word = document.getElementById("wordInput").value;
+        const desc = document.getElementById("descriptionInput").value;
+        this.reqStoreWord(word, desc);
+    }
+
+    reqSearchWord(word) {
+        this.xhttp.open("GET", "placeholderLink" + word, true);
+        this.xhttp.send();
+        
+        this.xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("searchOutputWrap").style.display = "block";
+                // Grab word, insert into document.getElementById("fetchedWord")
+                // Grab description, insert into document.getElementById("fetchedDesc")
+            }
+        };
+    }
+    
+    searchWord() {
+        const word = document.getElementById("searchInput").value;
+        this.reqSearchWord(word);
+    }
+}
+
 class UI {
     constructor(currLocation) {
+        this.xhr = new APIController();
         this.init(currLocation);
     }
 
@@ -19,12 +58,20 @@ class UI {
         document.getElementById("wordInputLabel").innerHTML = messages.storeWordInput;
         document.getElementById("descriptionInputLabel").innerHTML = messages.storeDescInput;
         document.getElementById("submitBtn").innerHTML = messages.submitBtn;
+        document.getElementById("submitBtn").addEventListener("click", (e) => {
+            e.preventDefault();
+            this.xhr.storeWord();
+        });
     }
 
     initSearch() {
         document.getElementById("prompt").innerHTML = messages.searchPrompt;
         document.getElementById("searchInput").placeholder = messages.searchInput;
         document.getElementById("searchBtn").innerHTML = messages.searchBtn;
+        document.getElementById("searchBtn").addEventListener("click", (e) => {
+            e.preventDefault();
+            this.xhr.searchWord();
+        })
     }
 }
 
